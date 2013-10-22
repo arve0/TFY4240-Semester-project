068760356cc7cca9deaf12f2c5c34c288f337403
Arve Seljebu <arve@probook.lan>
2013-10-22 23:39:40 +0200

    draw earth, mayavi example

--- src/model.py
+++ src/model.py
@@ -6,20 +6,7 @@ import matplotlib
 matplotlib.use('pdf')         # save figures as pdf
 from matplotlib import pyplot # pyplot as command
 
-def cartesianX(r,phi,theta):
-    'Calculate cartesian coordinates from sperical coordinates. Returns x.'
-    x = r*numpy.sin(theta)*numpy.cos(phi)
-    return x
 
-def cartesianY(r,phi,theta):
-    'Calculate cartesian coordinates from sperical coordinates. Returns y.'
-    y = r*numpy.cos(theta)*numpy.sin(phi)
-    return y
-
-def cartesianZ(r,phi,theta):
-    'Calculate cartesian coordinates from sperical coordinates. Returns z.'
-    z = r*numpy.cos(theta)
-    return z
 
 def main():
     # constants from wikipedia
@@ -33,18 +20,13 @@ def main():
     phi   = numpy.linspace(0, 2*numpy.pi, 100)
     theta = numpy.linspace(0, numpy.pi, 100)
     # earth coordinates
-    X = numpy.linspace(0,earthRadius,100)
-    X, Y = numpy.meshgrid(X, X)
-    Y = numpy.linspace(0,earthRadius,100)
-    Z = array([],[])
-    earthX = numpy.vectorize(cartesianX)
-    earthY = numpy.vectorize(cartesianY)
-    earthZ = numpy.vectorize(cartesianZ)
-    x = earthX(earthRadius, phi, theta)
-    y = earthY(earthRadius, phi, theta)
-    z = earthZ(earthRadius, phi, theta)
-    pyplot.contour(x, y, z)
-    pyplot.savefig()
+    x = earthRadius*numpy.cos(phi)
+    y = earthRadius*numpy.sin(phi)
+    pyplot.plot(x, y)
+    pyplot.savefig('earth')
+
+    # B-field
+    #B = numpy.arange
 
 if __name__ == '__main__':
     main()
